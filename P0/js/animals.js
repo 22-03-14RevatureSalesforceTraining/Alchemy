cf2fd6944ddb3d936cf8415b0a970bf4ec28a398
NamPhan56 <namp@umass.edu>
2022-03-17 23:49:10 -0400

    imported external js into index.html

---
+++ P0/js/animals.js
@@ -0,0 +1,20 @@
+
+function lions(){
+    console.log("lions!");
+}
+
+function elephants(){
+    console.log("elephants!");
+
+}
+
+function dolphins(){
+    console.log("dophins!");
+}
+
+function tuna(){
+    console.log("tuna!");
+}
+
+
+//export { lions, elephants, dophins, tuna };
