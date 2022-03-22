b26f646fd6e2e69e8d3f224d5c719ac1c299e030
Alfred Do <alfreddowork@gmail.com>
2022-03-21 22:55:40 -0700

    Final functionality updates
    Added youtube API integration, try catch, and user interaction. Missing comments now

--- Project0/javascript/project0.js
+++ Project0/javascript/project0.js
@@ -1,15 +1,8 @@
-document.getElementById("btn").addEventListener("click, changeColor");
+function myfunc(){
+    const test = document.getElementById("test");
+    test.style.backgroundColor = "black";
 
-var counter = 1;
-
-function changeColor(){
-    var colorID = document.getElementById(btn);
-    if(counter == 0){
-        colorID.style.backgroundColor = "#C0C0C0";
-        counter = 1;
-    } else {
-        colorID.style.backgroundColor = "#C55E5E";
-        counter = 1;
-    }
+    setTimeout(myfunc, 1000);
 
 }
+
